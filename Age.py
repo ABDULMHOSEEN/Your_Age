b57c3921df5d3139733e64ffa013bd322775277b
Mm <83562200+ABDULMHOSEEN@users.noreply.github.com>
2021-06-24 00:23:58 +0300

    Update Age.py

--- Age.py
+++ Age.py
@@ -1,12 +1,12 @@
 import datetime
 print("Hello I am (Age).")
-print("My function is to tell you yor age by taking some information")
+print("My function is to tell your age by taking some information")
 
 def age():
     ynow=datetime.datetime.now().year
     mnow=datetime.datetime.now().month
     dnow=datetime.datetime.now().day
-    y=ynow-int(input('inter the year that you was porn in'))
+    y=ynow-int(input('inter the year that you was born in'))
     m=(mnow-int(input("inter the month")))*28
     d=dnow-int(input('inter the day'))
     md = ((m+d)//29)
